61ec8b2f2229d71f44fd6b86a1be9b9cdb416b9a
mertakcay <kcaymer1@gmail.com>
2025-02-05 23:36:19 +0300

    Initial Commit

---
+++ stories/storiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  storiesApp.swift
+//  stories
+//
+//  Created by Mert Akçay on 5.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct storiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
